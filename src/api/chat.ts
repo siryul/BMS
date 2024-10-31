03c0656646934cc17260a5471544864ebb423abc
Xingyu <xingyu_kang@outlook.com>
2024-10-31 20:07:47 +0800

    feat: :sparkles: 完善api
    实现列出本地模型｜
    
    BREAKING CHANGE: 列出本地模型｜拉取模型｜推送模型｜删除模型｜查询模型信息

--- src/api/chat.ts
+++ src/api/chat.ts
@@ -1,12 +1,82 @@
-import type { IChatRes, IResponse } from '@/types'
+import type {
+  IModel,
+  IModelInfo,
+  IPResponse,
+  IPullPushParams,
+  IPullResponse,
+  IResponse,
+} from '@/types'
 import request from '@/utils/request'
+import type { AxiosError } from 'axios'
 
-export const chat = async (data: IChatRes) =>
-  request.post('/api/v1/chat/completions', data)
+/**
+ * 列出本地模型
+ * @returns {Promise<{ models: IModel[] }>} 返回一个包含模型数组的Promise对象
+ */
+export const listModel = async (): Promise<{ models: IModel[] }> => {
+  return request.get('/api/tags')
+}
 
+/**
+ * 生成响应
+ * @param {string} prompt - 用户输入的提示信息
+ * @param {string} model - 使用的模型名称
+ * @param {boolean} stream - 是否以流的方式返回数据，默认为 false
+ * @returns {Promise<IResponse>} 返回一个包含生成结果的Promise对象
+ */
 export const generate = async (
   prompt: string,
-  model: string = 'llama3.2',
+  model: string,
+  stream = false,
 ): Promise<IResponse> => {
-  return request.post('/api/generate', { prompt, model, stream: false })
+  return request.post('/api/generate', { prompt, model, stream })
+}
+
+/**
+ * 获取模型的详细信息
+ * @param {modelInfo} params - 包含模型名称和可选的详细信息标志的参数对象
+ * @param {string} params.name - 模型的名称
+ * @param {boolean} [params.verbose] - 是否获取详细信息，默认为 false
+ * @returns {Promise<any>} 返回一个包含模型信息的Promise对象
+ */
+export const modelInfo = async (params: IModelInfo): Promise<any> => {
+  return request.post('/api/show', params)
+}
+
+/**
+ * 删除指定名称的模型
+ * @param {string} name - 要删除的模型名称
+ * @returns {Promise<{err:string}>} 返回一个Promise对象，表示删除操作的完成与否，若错误消息为空，表示删除成功，反之失败
+ */
+export const deleteModel = async (name: string): Promise<{ err: string }> => {
+  try {
+    await request.delete(`/api/delete`, {
+      data: {
+        name,
+      },
+    })
+    return { err: '' }
+  } catch (error) {
+    return { err: 'Failed to delete' }
+  }
+}
+
+/**
+ * 拉取模型
+ * @param {IPullParams} params - 包含模型名称和可选参数的对象
+ * @param {string} params.name - 要拉取的模型名称
+ * @param {boolean} [params.insecure] - 是否允许不安全的连接，默认为 false
+ * @param {boolean} [params.stream] - 是否以流的方式拉取数据，默认为 false
+ * @returns {Promise<IPullResponse>} 返回一个包含拉取状态的Promise对象
+ */
+export const pullModel = async (
+  params: IPullPushParams,
+): Promise<IPullResponse> => {
+  return request.post('/api/pull', params)
+}
+
+export const pushModel = async (
+  params: IPullPushParams,
+): Promise<IPResponse> => {
+  return request.post('/api/push', params)
 }
--- src/types/index.ts
+++ src/types/index.ts
@@ -17,30 +17,51 @@ export interface IMessage {
   tool_calls?: string[] // 模型要使用的工具列表
 }
 
-// {
-//   "model": "llama3.2",
-//   "created_at": "2023-08-04T19:22:45.499127Z",
-//   "response": "The sky is blue because it is the color of the sky.",
-//   "done": true,
-//   "context": [1, 2, 3],
-//   "total_duration": 5043500667,
-//   "load_duration": 5025959,
-//   "prompt_eval_count": 26,
-//   "prompt_eval_duration": 325953000,
-//   "eval_count": 290,
-//   "eval_duration": 4709213000
-// }
-
 export interface IResponse {
   model: string
   created_at: string
-  response: string
+  response: null | string
   done: boolean
-  context: number[]
-  total_duration: number
-  load_duration: number
-  prompt_eval_count: number
-  prompt_eval_duration: number
-  eval_count: number
-  eval_duration: number
+  context?: number[] // 此响应中使用的对话的编码，可以在下一个请求中发送以保持对话记忆
+  total_duration?: number // 生成响应所花费的时间
+  load_duration?: number // 加载模型所花费的时间（以纳秒为单位）
+  prompt_eval_count?: number // 提示符中的令牌数
+  prompt_eval_duration?: number // 评估提示所花费的时间（以纳秒为单位）
+  eval_count?: number // 响应中的令牌数
+  eval_duration?: number // 生成响应所花费的时间（以纳秒为单位）
+}
+
+export interface IModel {
+  name: string
+  modified_at: string
+  size: number
+  digest: string
+  details: {
+    format: string
+    family: string
+    families: null | string[]
+    parameter_size: string
+    quantization_level: string
+  }
+}
+
+export interface IModelInfo {
+  name: string
+  verbose?: boolean
+}
+
+export interface IPullPushParams {
+  name: string
+  insecure?: boolean
+  stream?: boolean
+}
+
+export interface IPResponse {
+  status: string
+  digest?: string
+  total?: number
+}
+
+export interface IPullResponse extends IPResponse {
+  completed?: number
 }
